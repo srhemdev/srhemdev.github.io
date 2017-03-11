7096658d531fdfaacb8a1d25e33af16ed498529f
shwetahemdev <shweta.hemdev@elementum.com>
2017-03-11 13:37:09 -0800

    fixing styles

--- app/widgets/feedCard/feedCard.js
+++ app/widgets/feedCard/feedCard.js
@@ -3,11 +3,15 @@ var feedCard = (function(w, d){
         var vm = this;
 
         var feedCardElement = '<li data-card-id="'+ config.cardId +'" class="feed-card flex-row">\
-                                <img class="feed-card-image" src="'+ config.img +'" alt="No Image"/>\
+                                <div class="card-background-image">\
+                                    <img class="feed-card-image" src="'+ config.img +'" alt="No Image"/>\
+                                </div>\
                                 <div class="feed-card-content flex-column">\
                                     <div class="title">'+ config.title +'</div>\
-                                    <div class="subtitle">'+ config.subtitle +'</div>\
-                                    <div class="viewers">'+ config.viewers +'</div>\
+                                    <div class="subtitle">'+
+                                        (config.subtitle || '-') + ' | ' +
+                                        (config.viewers>=0 ? config.viewers : '-') +
+                                    '</div>\
                                     <div class="description">'+ config.description +'</div>\
                                 </div>\
                               </li>';
