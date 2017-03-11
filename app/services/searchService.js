f8a2a6198fcba1f0691cb2f4fccd7d5407eb098a
shwetahemdev <shweta.hemdev@elementum.com>
2017-03-10 17:20:21 -0800

    adding search service

--- app/services/searchService.js
+++ app/services/searchService.js
@@ -5,11 +5,16 @@ var searchService = (function(){
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText,"this.responseText")
+                foo(this.responseText)
                 //callback(JSON.parse(this.responseText));
             }
         };
-        xhttp.open("GET", url + query + '&callback=foo', true);
+        xhttp.open("GET", url + query + '&client_id=p7u367c89hr4ouomumvogv9hh5636f&callback=foo', true);
         xhttp.send();
+    }
+
+    function foo(res) {
+        console.log(foo);
     }
 
     return {
