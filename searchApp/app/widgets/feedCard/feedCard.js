d03e5245cc18ca990f26e9b4c4671bd0af66ece6
shwetahemdev <shweta.hemdev@elementum.com>
2017-03-15 23:11:21 -0700

    fix for pluralization

--- searchApp/app/widgets/feedCard/feedCard.js
+++ searchApp/app/widgets/feedCard/feedCard.js
@@ -32,7 +32,8 @@ var feedCard = (function(w, d){
                                     <a href="' + config.url + '" target="_blank">' + config.title + '</a></div>\
                                     <div class="subtitle">'+
                                         (config.subtitle || '-') + ' | ' +
-                                        (config.viewers>=0 ? config.viewers + ' viewers' : '-') +
+                                        (config.viewers>=0 ? config.viewers +
+                                        (config.viewers===1 ?' viewer': ' viewers'): '-') +
                                     '</div>\
                                     <div class="description">'+ config.description +'</div>\
                                 </div>\
